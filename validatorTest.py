ca462d6b047886aff878803d63738a4727c9cd00
Manoel <mancordosa@gmail.com>
2026-04-21 23:26:51 +0000

    Removi repeticao

--- validatorTest.py
+++ validatorTest.py
@@ -4,26 +4,26 @@ import validator
 
 
 class TestValidator(unittest.TestCase):
+    v = None
+
+    def setUp(self):
+        self.v = validator.Validator()
 
     def test_normal_url(self):
-        v = validator.Validator()
         url = "https://www.amazon.com.br/"
-        self.assertTrue(v.is_url(url))
+        self.assertTrue(self.v.is_url(url))
 
     def test_invalid_url(self):
-        v = validator.Validator()
         url = "asdfdghjkdfghkj"
-        self.assertFalse(v.is_url(url))
+        self.assertFalse(self.v.is_url(url))
 
     def test_empty_url(self):
-        v = validator.Validator()
         url = ""
-        self.assertFalse(v.is_url(url))
+        self.assertFalse(self.v.is_url(url))
 
     def test_not_http(self):
-        v = validator.Validator()
         url = "ftp://ftp.seusite.com.br"
-        self.assertFalse(v.is_url(url))
+        self.assertFalse(self.v.is_url(url))
 
 if __name__ == '__main__':
     unittest.main()
